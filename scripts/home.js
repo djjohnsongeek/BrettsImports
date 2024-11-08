b22fdaa9481c305297d51de3a6d404999ca2c38d
Daniel Johnson <Daniel.Johnson@greensboro-nc.gov>
2024-11-08 15:28:54 -0500

    feat: more additions

--- scripts/home.js
+++ scripts/home.js
@@ -2,10 +2,12 @@
 class HomePage {
 
     static startYear = 1990;
+    static slideIndex = 0;
 
     constructor()
     {
         this.setYearsOfExperiance();
+        HomePage.showSlides();
     }
 
 
@@ -13,6 +15,26 @@ class HomePage {
         const currentYear = new Date().getFullYear();
         const yearsOfExperiance = currentYear - HomePage.startYear;
         document.getElementById("years-of-experiance").innerText = `${yearsOfExperiance}`;
+    }
+
+    static showSlides() {
+        let slides = document.getElementsByClassName("mySlides");
+
+        // Hide All
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+
+        // Increment the index
+        HomePage.slideIndex++;
+        if (HomePage.slideIndex >= slides.length)
+        {
+            HomePage.slideIndex = 0;
+        }
+
+        slides[HomePage.slideIndex].style.display = "block";
+        console.log(slides[HomePage.slideIndex]);
+        setTimeout(HomePage.showSlides, 8000);
     }
 }
 

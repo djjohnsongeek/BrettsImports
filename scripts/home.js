80492c064767f6cd991f0046c06977aa96d1ccfe
Daniel Johnson <Daniel.Johnson@greensboro-nc.gov>
2024-11-15 07:12:19 -0500

    feat: launch the website

--- scripts/home.js
+++ scripts/home.js
@@ -3,41 +3,12 @@ class HomePage {
 
     static startYear = 1990;
     static slideIndex = 0;
-    static developerCode = "8alCi2u3NaqD";
-
-    constructionCompleteDiv;
-    underConstructionDiv;
-    submitCodeBtn;
-    codeInput;
 
     constructor()
     {
-        this.getElements();
-        this.constructionCompleteDiv.style.display = "none";
-        this.codeInput.addEventListener("change", () => {
-            this.codeInput.setAttribute("aria-invalid", "");
-        });
-        this.submitCodeBtn.addEventListener("click", () => {
-            if (this.codeInput.value === HomePage.developerCode)
-            {
-                this.underConstructionDiv.style.display = "none";
-                this.constructionCompleteDiv.style.display = "";
-            }
-            else {
-                this.codeInput.setAttribute("aria-invalid", true);
-            }
-        });
         this.setYearsOfExperiance();
         HomePage.showSlides();
     }
-
-    getElements() {
-        this.constructionCompleteDiv = document.getElementById("construction-complete-container");
-        this.underConstructionDiv = document.getElementById("under-construction-container");
-        this.codeInput = document.getElementById("code-input");
-        this.submitCodeBtn = document.getElementById("submit-code-btn");
-    }
-
 
     setYearsOfExperiance() {
         const currentYear = new Date().getFullYear();
